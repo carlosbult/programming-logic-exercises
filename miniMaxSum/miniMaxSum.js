fb5bce1e1e270974212e7f9d6e4d61036dbbe1ae
carlosbult <acevedo.bult@gmail.com>
2022-02-02 17:25:59 -0400

    miniMaxSum

---
+++ miniMaxSum/miniMaxSum.js
@@ -0,0 +1,19 @@
+function miniMaxSum(arr) {
+  let num = arr.sort((a, b) => a - b);
+
+  let min = num.slice(0, -1).reduce((e, i) => e + i);
+
+  let max = num.slice(1).reduce((e, i) => e + i);
+
+  console.log(min, max);
+}
+
+let array0 = [1, 3, 5, 7, 9];
+let array1 = [1, 2, 3, 4, 5];
+let array2 = [3, 5, 7, 95, -4, 1];
+let array3 = [7, 69, 2, 221, 8974];
+
+miniMaxSum(array0);
+miniMaxSum(array1);
+miniMaxSum(array2);
+miniMaxSum(array3);
