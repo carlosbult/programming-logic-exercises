04a3a599ca41b8f4db4aa296ddf32834e40caab3
carlosbult <acevedo.bult@gmail.com>
2022-02-15 08:28:14 -0400

    CamelCase4 Excersim

---
+++ CamelCase4/processData.js
@@ -0,0 +1,50 @@
+function processData(input) {
+  input.forEach((element) => {
+    let commands = element.substring(0, 4);
+    let restOfWord = element.substring(4);
+    let inputString = [];
+    const regexCap = /[A-Z]/g;
+    if (restOfWord.endsWith('()')) {
+      restOfWord = restOfWord.slice(0, restOfWord.length - 2);
+    }
+    for (let i = 0; i < restOfWord.length; i++) {
+      if (restOfWord[i].match(regexCap)) {
+        inputString.push(' ');
+        inputString.push(restOfWord[i].toLowerCase());
+      } else {
+        inputString.push(restOfWord[i]);
+      }
+    }
+    let newInput = inputString.join('').trim().split(' ');
+    //Case S
+    if (commands[0] === 'S') {
+      console.log(newInput.join(' ').trim());
+    }
+    // Case C
+    if (commands[0] === 'C') {
+      let newString = [];
+      newInput.forEach((e) => {
+        newString.push(e[0].toUpperCase() + e.substring(1));
+      });
+      let firstLow = newString[0][0].toLowerCase() + newString[0].substring(1);
+      let result = firstLow + newString.slice(1).join('');
+      if (commands[2] === 'V') {
+        console.log(result);
+      } else if (commands[2] === 'M') {
+        console.log(`${result}()`);
+      } else {
+        console.log(newString.join(''));
+      }
+    }
+  });
+}
+
+let first = [
+  'S;V;iPad',
+  'C;M;mouse pad',
+  'C;C;code swarm',
+  'S;C;OrangeHighlighter',
+];
+
+processData(first);
+
